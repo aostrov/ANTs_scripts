ed130d1bb1b9565d6f038ba5d61332fccd8978c8
Aaron Ostrovsky <aarondostrovsky+gitfork@gmail.com>
2021-04-28 14:38:02 +0100

    simplify antsTransformations.sh to more gracefully handle bridging transformations

--- antsTransformation.sh
+++ antsTransformation.sh
@@ -2,35 +2,19 @@
 
 # Apply generated transforms to register to reference
 
-if [[ $bridging -eq 0 ]] ; then
-	${ANTs_path}/antsApplyTransforms \
-		-d 3 \
-		-v 1 \
-		--float 1\
-		-n WelchWindowedSinc \
-		-f 0 \
-		-i ${nthChannelIn} \
-		-r ${fixed} \
-		-o registration/${nthChannelOut} \
-		-t registration/${output}1Warp.nii.gz \
-		-t registration/${output}0GenericAffine.mat
-elif [[ $bridging -eq 1 ]] ; then
-	${ANTs_path}/antsApplyTransforms \
-		-d 3 \
-		-v 1 \
-		--float 1\
-		-n WelchWindowedSinc \
-		-f 0 \
-		-i ${nthChannelIn} \
-		-r ${atlas} \
-		-o ${nthChannelOut} \
-		-t registration/${bridging_warp} \
-		-t registration/${output}1Warp.nii.gz \
-		-t registration/${output}0GenericAffine.mat
-else
-	echo "You've hit some weird state."
-	echo "Bridging should only have a value of 1 or 0"
-fi
+${ANTs_path}/antsApplyTransforms \
+	-d 3 \
+	-v 1 \
+	--float 1 \
+	-n WelchWindowedSinc \
+	-f 0 \
+	-i ${nthChannelIn} \
+	-r ${fixed} \
+	-o registration/${nthChannelOut} \
+	-t ${final_transformation_files}
+
+# registration/${output}1Warp.nii.gz \
+# 	-t registration/${output}0GenericAffine.mat
 
 
 # Convert to 16bit from 32bit
